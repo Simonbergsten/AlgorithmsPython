c81fd3fedd7a557c751074f5e2301e0fabac9aec
simon <simon.bergsten@hotmail.com>
2020-11-29 22:18:20 +0100

    Added recursion

---
+++ Algorithms/Recursion/Subsets.py
@@ -0,0 +1,21 @@
+"""
+
+"""
+
+class Solution:
+    def solution(self, nums, ans, cur, index):
+        if (index > len(nums)):
+            return
+        ans.append(cur[:])
+        for i in range(index, len(nums)):
+            if (nums[i] not in cur):
+                cur.append(nums[i])
+                self.solution(nums, ans, cur, i)
+                cur.pop()
+
+
+    def subset(self, nums):
+        ans = []
+        cur = []
+        self.solution(nums, ans, cur, 0)
+        return ans
